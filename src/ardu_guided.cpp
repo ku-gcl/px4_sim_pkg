6b84ac96900c4fa0c5ee8fc2d51a2ad9c47a1bdc
SHIO777 <shiotatsu1015@gmail.com>
2022-12-13 13:46:08 +0900

    [add] height to 0.5

--- src/ardu_guided.cpp
+++ src/ardu_guided.cpp
@@ -220,7 +220,8 @@ int main(int argc, char **argv)
     {
         ROS_INFO("CIRCLE");
         omega = 0.6;
-        HEIGHT = 1.5;
+        // HEIGHT = 1.5;
+        HEIGHT = 0.5;
         setDestination(1.0, 0, HEIGHT);
     }
     else if (MODE == "updown")
