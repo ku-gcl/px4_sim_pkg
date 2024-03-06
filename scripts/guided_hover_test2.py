8d0df10dc95ab9812c1ca8bc9f410271556d3c05
SHIO777 <shiotatsu1015@gmail.com>
2024-03-06 13:41:24 +0900

    update

--- scripts/guided_hover_test2.py
+++ scripts/guided_hover_test2.py
@@ -4,65 +4,62 @@ from std_msgs.msg import Float64
 from geometry_msgs.msg import PoseStamped
 from geographic_msgs.msg import GeoPointStamped
 
-def main():
-    # ノードの初期化後の処理や変数の設定
-    rospy.init_node('offb_node', anonymous=True)
-    mavros_node = MavrosNode.MavrosNode()
-    rate = rospy.Rate(10.0)    
-    MODE = "hovering"  # 飛行モードの選択: circle, updown, eight, hovering
 
-    # FCUの接続を待つ
-    while not mavros_node.current_state.connected:
-        rospy.sleep(0.1)
-    
-    rospy.loginfo("Waiting for connection...")
+# ノードの初期化後の処理や変数の設定
+rospy.init_node('offb_node', anonymous=True)
+mavros_node = MavrosNode.MavrosNode()
+rate = rospy.Rate(10.0)    
+MODE = "hovering"  # 飛行モードの選択: circle, updown, eight, hovering
 
-    # グローバルポジションの原点を設定
-    rospy.loginfo("Set Global Position ...")
-    for i in range(10):
-        mavros_node.set_gp_position(latitude=33.595270, longitude=130.215496)
-        rate.sleep()
-    rospy.loginfo("Done Global Position ...")
+# FCUの接続を待つ
+while not mavros_node.current_state.connected:
+    rospy.sleep(0.1)
 
-    # GYM_OFFSETの計算
-    rospy.sleep(3)  # 現在のheadingを取得するための時間
-    mavros_node.set_gym_offset()
+rospy.loginfo("Waiting for connection...")
 
-    # 開始地点をlocal座標で設定
-    mavros_node.set_destination(x=0, y=0, z=0.5)
-    mavros_node.pub_local_position()
+# グローバルポジションの原点を設定
+rospy.loginfo("Set Global Position ...")
+for i in range(10):
+    mavros_node.set_gp_position(latitude=33.595270, longitude=130.215496)
+    rate.sleep()
+rospy.loginfo("Done Global Position ...")
 
-    # GUIDEDモードに変更
-    mavros_node.set_drone_to_guided_mode_manual()
-    # mavros_node.set_drone_to_guided_mode_auto()
+# GYM_OFFSETの計算
+rospy.sleep(3)  # 現在のheadingを取得するための時間
+mavros_node.set_gym_offset()
 
-    # 機体をアーム
-    mavros_node.arm_vehicle()
+# 開始地点をlocal座標で設定
+mavros_node.set_destination(x=0, y=0, z=0.5)
+mavros_node.pub_local_position()
 
-    # 離陸
-    mavros_node.vehicle_takeoff(0.5)
+# GUIDEDモードに変更
+mavros_node.set_drone_to_guided_mode_manual()
+# mavros_node.set_drone_to_guided_mode_auto()
 
-    rospy.sleep(5)  # 安定を待つ
-    mavros_node.set_heading(0)
+# 機体をアーム
+mavros_node.arm_vehicle()
 
-    # 特定の期間ホバリングを実行
-    start_time = rospy.Time.now()
-    duration = 20.0
-    rate_ctrl = rospy.Rate(20)
-    HEIGHT = 0.5
-    
-    rospy.loginfo("HOVERING")
-    while not rospy.is_shutdown() and (rospy.Time.now() - start_time) < rospy.Duration(duration):
-        if MODE == "hovering":
-            mavros_node.set_destination(x=0, y=0, z=HEIGHT)
-            # mavros_node.set_local_position(x=0, y=0, z=HEIGHT)
-            mavros_node.pub_local_position()
-            
-        rate_ctrl.sleep()  # 0.1秒ごとにループ
-    rospy.loginfo("end hovering")
+# 離陸
+mavros_node.vehicle_takeoff(0.5)
 
-    # 着陸
-    mavros_node.send_land_command()
+rospy.sleep(5)  # 安定を待つ
+mavros_node.set_heading(0)
 
-if __name__ == '__main__':
-    main()
+# 特定の期間ホバリングを実行
+start_time = rospy.Time.now()
+duration = 20.0
+rate_ctrl = rospy.Rate(20)
+HEIGHT = 0.5
+
+rospy.loginfo("HOVERING")
+while not rospy.is_shutdown() and (rospy.Time.now() - start_time) < rospy.Duration(duration):
+    if MODE == "hovering":
+        mavros_node.set_destination(x=0, y=0, z=HEIGHT)
+        # mavros_node.set_local_position(x=0, y=0, z=HEIGHT)
+        mavros_node.pub_local_position()
+        
+    rate_ctrl.sleep()  # 0.1秒ごとにループ
+rospy.loginfo("end hovering")
+
+# 着陸
+mavros_node.send_land_command()
