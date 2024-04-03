86261cc9e1ad64e9e8a4dae259983afdd2cdf572
SHIO777 <shiotatsu1015@gmail.com>
2024-04-03 12:41:51 +0900

    add: test for confirming python's length behavior

---
+++ scripts/test/test_length.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+## 
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+
+##
+data = []
+for i in range(3):
+    a = [1, 2, 3]
+    data.append(a)
+print(data)
+print(len(data))
