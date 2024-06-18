d7275516ad8ff515af3f18979c3264831f6cf5c1
Tat <hello@example.com>
2024-06-18 16:35:18 +0900

    update: test modify

--- scripts/test/test_rtdmd.py
+++ scripts/test/test_rtdmd.py
@@ -30,8 +30,8 @@ inputDim, _ = u.shape
 delta = 10**(-3)
 lam = 0.97
 
-aug = 2*stateDim+1
-# aug = 2
+# aug = 2*stateDim+1
+aug = 2
 # aug = 1
 
 # RTDMD初期化
@@ -83,12 +83,75 @@ for i in range(NV-1):
     dmdlog[i] = rtdmd.rtdmdResult
     prevData = newData
 
-
 # %%
 # 予測前の1~aug列までは0埋めしておく
 xPredictV = np.concatenate((np.zeros((stateDim, aug)), xPredictV[:, 1:]), axis=1)
-
 # 誤差計算
 ErrorV = rtdmd.calculate_fit_error(test_data_y[:, aug+1:], xPredictV[:, aug+1:])
-
 print(ErrorV)
+
+
+# 逐次的にデータを追加してRTDMDを更新
+YV = dmd.concatenate(test_data_y, test_data_u)
+
+rtdmd2 = RTDMD.RTDMD(delta, lam, aug, stateDim, inputDim)
+rtdmd2.rtdmdResult["AB"] = A
+prevData = np.zeros(rtdmd2.augStateInputDim)
+newData = np.zeros(rtdmd2.augStateInputDim)
+# ログ記録用
+dmdlog = []
+xPredictV2 = []
+
+[_, NV] = test_data_y.shape
+
+for i in range(NV):
+    # get sensordata and inputdata
+    state_data = test_data_y[:, i]
+    input_data = test_data_u[:, i]
+    
+    if i < aug:
+        newData[i*stateDim:(i+1)*stateDim] = state_data
+        u_start = stateDim * aug
+        newData[u_start+i*inputDim:u_start+(i+1)*inputDim] = input_data
+        continue
+
+    # store prevData
+    prevData = newData
+
+    # decompose data to state and input
+    aug_state_data = prevData[:aug*stateDim]
+    aug_input_data = prevData[aug*stateDim:]
+
+    # shift
+    aug_state_data[:(aug-1)*stateDim] = aug_state_data[stateDim:]
+    aug_state_data[(aug-1)*stateDim:] = state_data
+
+    aug_input_data[:(aug-1)*inputDim] = aug_input_data[inputDim:]
+    aug_input_data[(aug-1)*inputDim:] = input_data
+
+    newData = np.concatenate([aug_state_data, aug_input_data], axis=0)
+
+    # state_shift_row = (aug-1)*stateDim
+    # input_shift_row = (aug-1)*inputDim + state_shift_row
+    
+    # # シフト
+    # newData[:state_shift_row] = prevData[stateDim:state_shift_row + stateDim]
+    # newData[state_shift_row:input_shift_row] = prevData[state_shift_row + inputDim:input_shift_row + inputDim]
+    
+    # # 新しいデータを追加
+    # newData[state_shift_row:state_shift_row+stateDim] = state_data
+    # # newData[input_shift_row:input_shift_row+inputDim] = input_data
+    # newData[state_shift_row+stateDim:state_shift_row+stateDim+input_shift_row+inputDim] = input_data
+
+    rtdmd2.update_rtdmd(prevData, newData, i+1)
+    xPredictV2.append(rtdmd2.rtdmdResult['y_hat'])
+
+    dmdlog.append(rtdmd.rtdmdResult)
+    
+# xPredictV2リストをNumPy配列に変換
+xPredictV2 = np.array(xPredictV2).T
+# 予測前の1~aug列までは0埋めしておく
+xPredictV2 = np.concatenate((np.zeros((stateDim, aug)), xPredictV2[:, 0:]), axis=1)
+# 誤差計算
+ErrorV2 = rtdmd2.calculate_fit_error(test_data_y[:, aug:], xPredictV2[:, aug:])
+print(ErrorV2)
